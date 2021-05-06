cf9a42ff95db0c5e9b68f7d1357c2236000be572
CodeForFunUIT <nghiathdev@gmail.com>
2021-05-06 21:33:17 +0700

    19521902_đọc dữ liệu của quảng cáo từ database

--- app/src/main/java/com/hieunghia/dmt/appnghenhac/Service/DataService.java
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/Service/DataService.java
@@ -1,6 +1,16 @@
 package com.hieunghia.dmt.appnghenhac.Service;
 
+import com.hieunghia.dmt.appnghenhac.Model.QuangCao;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface DataService {
+
+    @GET("songbanner.php")
+    Call<List<QuangCao>> GetDataBanner();
 
     
 }
--- app/src/main/java/com/hieunghia/dmt/appnghenhac/fragment/Fragment_Banner.java
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/fragment/Fragment_Banner.java
@@ -1,6 +1,7 @@
 package com.hieunghia.dmt.appnghenhac.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.hieunghia.dmt.appnghenhac.Model.QuangCao;
 import com.hieunghia.dmt.appnghenhac.R;
+import com.hieunghia.dmt.appnghenhac.Service.APIService;
+import com.hieunghia.dmt.appnghenhac.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Fragment_Banner extends Fragment {
     View view;
@@ -17,6 +28,25 @@ public class Fragment_Banner extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_banner, container, false);
+        GetData();
         return view;
+    }
+
+    private void GetData() {
+        DataService dataService = APIService.getService(); //
+
+        Call<List<QuangCao>> callback = dataService.GetDataBanner();
+        callback.enqueue(new Callback<List<QuangCao>>() { // sự kiện lắng nghe
+            @Override
+            public void onResponse(Call<List<QuangCao>> call, Response<List<QuangCao>> response) { //lắng nghe có kết quả trả về
+                ArrayList<QuangCao> banners = (ArrayList<QuangCao>) response.body();
+                Log.d("BBB",banners.get(0).getTenBaiHat());
+            }
+
+            @Override
+            public void onFailure(Call<List<QuangCao>> call, Throwable t) { // thất bại
+
+            }
+        });
     }
 }
