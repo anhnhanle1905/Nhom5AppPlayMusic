f6c0e4efd9963312cf9414fb05969b2b204f7844
CodeForFunUIT <nghiathdev@gmail.com>
2021-05-07 23:39:42 +0700

    19521902_Thiết kế lại phần quảng cáo(hiển thị phần hình ảnh quảng cáo và icon hình ảnh quảng cáo)

---
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/Adapter/BannerAdapter.java
@@ -0,0 +1,66 @@
+package com.hieunghia.dmt.appnghenhac.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.hieunghia.dmt.appnghenhac.Model.QuangCao;
+import com.hieunghia.dmt.appnghenhac.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class BannerAdapter extends PagerAdapter {
+
+    Context context;
+    ArrayList<QuangCao> arrayListbanner;
+
+    public BannerAdapter(Context context, ArrayList<QuangCao> arrayListbanner){
+        this.context = context;
+        this.arrayListbanner = arrayListbanner;
+    }
+
+    @Override
+    public int getCount() { // mảng có bao nhiêu tấm hình thì vẽ bấy nhiêu
+        return arrayListbanner.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.dong_banner, null);
+
+        ImageView imgbackgroundbanner = view.findViewById(R.id.imageviewbackgroundbanner);
+        ImageView imgsongbanner = view.findViewById(R.id.imageviewbanner);
+        TextView txttitlesongbanner = view.findViewById(R.id.textviewtitlebanner);
+        TextView txtnoidung = view.findViewById(R.id.textviewnoidung);
+
+        Picasso.with(context).load(arrayListbanner.get(position).getHinhAnh()).into(imgbackgroundbanner); // gắn hình ảnh vào thư viện picasso (phải add vào)
+        Picasso.with(context).load(arrayListbanner.get(position).getHinhAnh()).into(imgsongbanner);
+        txttitlesongbanner.setText(arrayListbanner.get(position).getTenBaiHat());
+        txtnoidung.setText(arrayListbanner.get(position).getNoiDung());
+
+        container.addView(view);
+        return view;
+
+    }
+
+    // chuyển đến banner cuối cùng mà không finish sẽ bị lỗi
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/hieunghia/dmt/appnghenhac/fragment/Fragment_Banner.java
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/fragment/Fragment_Banner.java
@@ -1,6 +1,7 @@
 package com.hieunghia.dmt.appnghenhac.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +10,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.hieunghia.dmt.appnghenhac.Adapter.BannerAdapter;
 import com.hieunghia.dmt.appnghenhac.Model.QuangCao;
 import com.hieunghia.dmt.appnghenhac.R;
 import com.hieunghia.dmt.appnghenhac.Service.APIService;
@@ -18,18 +21,31 @@ import com.hieunghia.dmt.appnghenhac.Service.DataService;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Fragment_Banner extends Fragment {
     View view;
+    ViewPager viewPager;
+    CircleIndicator circleIndicator;
+    BannerAdapter bannerAdapter;
+    Runnable runnable;
+    Handler handler;
+    int currentItem;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_banner, container, false);
+        AnhXa();
         GetData();
         return view;
+    }
+
+    private void AnhXa() {
+        viewPager = view.findViewById(R.id.viewpaper);
+        circleIndicator = view.findViewById(R.id.indicatordefault);
     }
 
     private void GetData() {
@@ -40,7 +56,26 @@ public class Fragment_Banner extends Fragment {
             @Override
             public void onResponse(Call<List<QuangCao>> call, Response<List<QuangCao>> response) { //lắng nghe có kết quả trả về
                 ArrayList<QuangCao> banners = (ArrayList<QuangCao>) response.body();
-                Log.d("BBB",banners.get(0).getTenBaiHat());
+                bannerAdapter = new BannerAdapter(getActivity(), banners);
+                viewPager.setAdapter(bannerAdapter);
+                circleIndicator.setViewPager(viewPager);
+                handler = new Handler();
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        currentItem = viewPager.getCurrentItem();
+                        currentItem++;
+                        if (currentItem >= viewPager.getAdapter().getCount())
+                        {
+                            currentItem = 0;
+                        }
+                        viewPager.setCurrentItem(currentItem, true);
+                        handler.postDelayed(runnable,5000 ); // sau 5s nó sẽ tự động lướt qua
+
+                    }
+                };
+                handler.postDelayed(runnable, 5000);
+
             }
 
             @Override
