f0e180cde069778728e8bfd9a98127ef85071f92
CodeForFunUIT <nghiathdev@gmail.com>
2021-05-09 09:40:15 +0700

    19521902_đọc dữ liệu của playlist từ phía service

---
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/Model/PlayList.java
@@ -0,0 +1,52 @@
+package com.hieunghia.dmt.appnghenhac.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PlayList {
+
+    @SerializedName("IDPlaylist")
+    @Expose
+    private String iDPlaylist;
+    @SerializedName("Ten")
+    @Expose
+    private String ten;
+    @SerializedName("HinhNen")
+    @Expose
+    private String hinhNen;
+    @SerializedName("HinhIcon")
+    @Expose
+    private String hinhIcon;
+
+    public String getIDPlaylist() {
+        return iDPlaylist;
+    }
+
+    public void setIDPlaylist(String iDPlaylist) {
+        this.iDPlaylist = iDPlaylist;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getHinhNen() {
+        return hinhNen;
+    }
+
+    public void setHinhNen(String hinhNen) {
+        this.hinhNen = hinhNen;
+    }
+
+    public String getHinhIcon() {
+        return hinhIcon;
+    }
+
+    public void setHinhIcon(String hinhIcon) {
+        this.hinhIcon = hinhIcon; }
+
+}
--- app/src/main/java/com/hieunghia/dmt/appnghenhac/Service/DataService.java
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/Service/DataService.java
@@ -1,5 +1,8 @@
 package com.hieunghia.dmt.appnghenhac.Service;
 
+import android.provider.MediaStore;
+
+import com.hieunghia.dmt.appnghenhac.Model.PlayList;
 import com.hieunghia.dmt.appnghenhac.Model.QuangCao;
 
 import java.util.List;
@@ -12,5 +15,6 @@ public interface DataService {
     @GET("songbanner.php")
     Call<List<QuangCao>> GetDataBanner();
 
-    
+    @GET("playlistforcurent.php")
+    Call<List<PlayList>> GetPlayListCurrentDay();
 }
