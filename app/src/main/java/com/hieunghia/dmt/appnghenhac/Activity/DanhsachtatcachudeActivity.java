789ab0ece3763d62a4ee4ef3b494e4075aacb36e
CodeForFunUIT <nghiathdev@gmail.com>
2021-05-13 18:01:16 +0700

    19521902_Đọc_dữ_iệu_API_của_tất_cả_chủ_đề_Thiết_kế_giao_diện_cho_màn_hình_chứa_tất_cả_chủ_đề

---
+++ app/src/main/java/com/hieunghia/dmt/appnghenhac/Activity/DanhsachtatcachudeActivity.java
@@ -0,0 +1,76 @@
+package com.hieunghia.dmt.appnghenhac.Activity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.hieunghia.dmt.appnghenhac.Adapter.DanhsachtatcachudeAdapter;
+import com.hieunghia.dmt.appnghenhac.Model.ChuDe;
+import com.hieunghia.dmt.appnghenhac.R;
+import com.hieunghia.dmt.appnghenhac.Service.APIService;
+import com.hieunghia.dmt.appnghenhac.Service.DataService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DanhsachtatcachudeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerViewtatcachude;
+    Toolbar toolbartatcachude;
+    DanhsachtatcachudeAdapter danhsachtatcachudeAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danhsachtatcachude);
+        init();
+        GetDaTa();
+    }
+
+    private void GetDaTa() {
+        DataService dataService = APIService.getService();
+        Call<List<ChuDe>> callback = dataService.GetAllChuDe();
+        callback.enqueue(new Callback<List<ChuDe>>() {
+            @Override
+            public void onResponse(Call<List<ChuDe>> call, Response<List<ChuDe>> response) {
+                ArrayList<ChuDe> mangchude = (ArrayList<ChuDe>) response.body();
+                danhsachtatcachudeAdapter = new DanhsachtatcachudeAdapter(DanhsachtatcachudeActivity.this,mangchude);
+                recyclerViewtatcachude.setLayoutManager(new GridLayoutManager(DanhsachtatcachudeActivity.this,1));
+                recyclerViewtatcachude.setAdapter(danhsachtatcachudeAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<ChuDe>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public boolean onOptionsItemSelected( MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void init() {
+        recyclerViewtatcachude = findViewById(R.id.recyclerviewallchude);
+        toolbartatcachude = findViewById(R.id.toolbarallchude);
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+}
